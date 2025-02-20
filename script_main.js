3b90ac926e42c2b11c47810d965945293fe07a3b
LakshayChhabra248 <168405962+LakshChhabra248@users.noreply.github.com>
2025-02-20 14:22:21 +0530

    Hack KRMU

---
+++ script_main.js
@@ -0,0 +1,6 @@
+const signIn = document.getElementById('sign-in-button')
+const signUp = document.getElementById('sign-up-button')
+
+signIn.addEventListener('click', () => {
+    window.location.href = "html/login.html"
+})
